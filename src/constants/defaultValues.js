946789e27b12623824410a54cfffe56997af3934
Lina Alexandrova <ang.s.alexandrova@gmail.com>
2020-10-23 20:43:00 +0300

    Add from for login/registration and simple validation

--- src/constants/defaultValues.js
+++ src/constants/defaultValues.js
@@ -1,8 +1,20 @@
-export const LOCAL_STORAGE_TOKEN_NAME = "access_token";
+export const LOCAL_STORAGE_TOKEN = "access_token";
 export const LOCAL_STORAGE_USER = "insta_user";
 
 export const responseStatuses = {
   BAD_REQUEST: 400,
   UNAUTHORIZED: 401,
-  NOT_FOUND: 404
-}
+  NOT_FOUND: 404,
+};
+
+export const validateRegData = ({ email, password }) => {
+  const fieldsErrors = {};
+  if (password.length < 6)
+    fieldsErrors.password = "Password must be 6 characters or longer.";
+  if (!password) fieldsErrors.password = "Please enter your password.";
+  const pattern = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
+  if (!email.match(pattern)) fieldsErrors.email = "Email address is invalid";
+  if (!email && email !== 0)
+    fieldsErrors.email = "Please enter your email address";
+  return Object.keys(fieldsErrors).length ? fieldsErrors : false;
+};
--- src/views/user/auth/index.js
+++ src/views/user/auth/index.js
@@ -1,7 +1,101 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  Col,
+  FormFeedback,
+} from "reactstrap";
+import styles from "./index.module.scss";
+import { loginUser, registerUser } from "../../../redux/auth/actions";
+import { useDispatch } from "react-redux";
+import { validateRegData } from "../../../constants/defaultValues";
 
-const Authorization = () => {
-  return <div>Authorization Form</div>;
+const Authorization = ({ history, match }) => {
+  const [data, setData] = useState({ email: "", password: "" });
+  const [validateErrors, setValidateErrors] = useState({});
+  const [isRegisteredBefore, setIsRegisteredBefore] = useState(true);
+
+  const dispatch = useDispatch();
+  const onInputChange = (event) => {
+    setValidateErrors({});
+    setData({ ...data, [event.target.name]: event.target.value });
+  };
+  const handleNotRegClick = () => {
+    setValidateErrors({});
+    setIsRegisteredBefore((prevValue) => !prevValue);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const errors = validateRegData(data);
+    if (errors) {
+      setValidateErrors(errors);
+    } else {
+      isRegisteredBefore
+        ? dispatch(loginUser(data, history, match.url))
+        : dispatch(registerUser(data, history, match.url));
+    }
+  };
+  const { email, password } = data;
+  return (
+    <div className={styles.page_wrapper}>
+      <div className={styles.outer_wrapper}>
+        <Col>
+          <Card>
+            <CardHeader className={styles.form_header}>
+              {isRegisteredBefore ? "Login" : "Register"}
+            </CardHeader>
+            <CardBody className={styles.inner_wrapper}>
+              <Form className="d-flex flex-column">
+                <FormGroup>
+                  <Label>Email</Label>
+                  <Input
+                    type="email"
+                    name="email"
+                    value={email}
+                    invalid={!!validateErrors.email}
+                    onChange={(event) => onInputChange(event)}
+                  />
+                  <FormFeedback invalid="true" className="z-index-2">
+                    {validateErrors.email}
+                  </FormFeedback>
+                </FormGroup>
+                <FormGroup>
+                  <Label>Password</Label>
+                  <Input
+                    type="password"
+                    name="password"
+                    value={password}
+                    invalid={!!validateErrors.password}
+                    onChange={(event) => onInputChange(event)}
+                  />
+                  <FormFeedback invalid="true" className="z-index-2">
+                    {validateErrors.password}
+                  </FormFeedback>
+                </FormGroup>
+              </Form>
+              <div className="d-flex justify-content-between">
+                <Button onClick={handleNotRegClick} color="warning">
+                  {isRegisteredBefore
+                    ? "Not Registered Yet?"
+                    : "Already Registered?"}
+                </Button>
+                <Button onClick={handleSubmit} color="success">
+                  Submit
+                </Button>
+              </div>
+            </CardBody>
+          </Card>
+        </Col>
+      </div>
+    </div>
+  );
 };
 
 export default Authorization;
